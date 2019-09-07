9368bcce4445246added2fcbd1543e680b11395f
kristoffer <karlsson.kristoffer@gmail.com>
2019-09-07 21:56:39 +0200

    Remove call counting.

--- test/Ternacode.Persistence.EntityFrameworkCore.UnitTest/Fakes/DbContext_Fake.cs
+++ test/Ternacode.Persistence.EntityFrameworkCore.UnitTest/Fakes/DbContext_Fake.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -7,7 +6,6 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Ternacode.Persistence.EntityFrameworkCore.UnitTest.Fakes
 {
-    // TODO: Use NSubstitute
     public class DbContext_Fake : DbContext
     {
         private readonly IDictionary<string, int> _calls = new Dictionary<string, int>();
@@ -20,22 +18,9 @@ namespace Ternacode.Persistence.EntityFrameworkCore.UnitTest.Fakes
         public override DatabaseFacade Database { get; }
 
         public override int SaveChanges()
-            => CountCall<int>();
+            => default;
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-            => Task.FromResult(CountCall<int>());
-
-        public int GetCallCount(string methodName)
-            => _calls[methodName];
-
-        private T CountCall<T>([CallerMemberName] string caller = null, T value = default)
-        {
-            if (!_calls.TryGetValue(caller, out _))
-                _calls.Add(caller, 0);
-
-            _calls[caller]++;
-
-            return value;
-        }
+            => Task.FromResult(default(int));
     }
 }
