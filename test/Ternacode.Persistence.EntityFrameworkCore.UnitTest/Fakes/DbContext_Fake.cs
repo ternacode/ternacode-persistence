c90240f243c60a63794674788461016bea3b4116
kristoffer <karlsson.kristoffer@gmail.com>
2019-09-07 21:58:07 +0200

    Remove call counting from unit of work. Remove unused field.

--- test/Ternacode.Persistence.EntityFrameworkCore.UnitTest/Fakes/DbContext_Fake.cs
+++ test/Ternacode.Persistence.EntityFrameworkCore.UnitTest/Fakes/DbContext_Fake.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
@@ -8,8 +7,6 @@ namespace Ternacode.Persistence.EntityFrameworkCore.UnitTest.Fakes
 {
     public class DbContext_Fake : DbContext
     {
-        private readonly IDictionary<string, int> _calls = new Dictionary<string, int>();
-
         public DbContext_Fake()
         {
             Database = new DatabaseFacade_Fake(this);
