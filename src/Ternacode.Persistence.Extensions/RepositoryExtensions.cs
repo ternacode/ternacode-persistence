b006604a72b68f9e710a8cdd9890324816c7a7a6
kristoffer <karlsson.kristoffer@gmail.com>
2020-10-12 15:53:40 +0200

    Add extensions project.

---
+++ src/Ternacode.Persistence.Extensions/RepositoryExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ternacode.Persistence.Abstractions;
+
+namespace Ternacode.Persistence.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static async Task<IEnumerable<TEntity>> QueryAsync<TEntity>(
+            this IRepository<TEntity> repository,
+            Func<IQueryable<TEntity>, IQueryable<TEntity>> queryFunc)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (queryFunc == null)
+            {
+                throw new ArgumentNullException(nameof(queryFunc));
+            }
+
+            return await repository.QueryAsync(new FuncQuery<TEntity>(queryFunc));
+        }
+
+        public static async Task<IEnumerable<TEntity>> QueryAsync<TEntity>(
+            this IRepository<TEntity> repository,
+            Func<IQueryable<TEntity>, IQueryable<TEntity>> queryFunc,
+            IEnumerable<string> loadedPropertyNames)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (queryFunc == null)
+            {
+                throw new ArgumentNullException(nameof(queryFunc));
+            }
+
+            if (loadedPropertyNames == null)
+            {
+                throw new ArgumentNullException(nameof(loadedPropertyNames));
+            }
+
+            return await repository.QueryAsync(new FuncQuery<TEntity>(queryFunc, loadedPropertyNames));
+        }
+
+        private class FuncQuery<TEntity> : BaseQuery<TEntity>
+        {
+            private readonly Func<IQueryable<TEntity>, IQueryable<TEntity>> _queryFunc;
+            private readonly IEnumerable<string> _loadedPropertyNames;
+
+            public FuncQuery(Func<IQueryable<TEntity>, IQueryable<TEntity>> queryFunc)
+                => _queryFunc = queryFunc;
+
+            public FuncQuery(
+                Func<IQueryable<TEntity>, IQueryable<TEntity>> queryFunc,
+                IEnumerable<string> loadedPropertyNames)
+            {
+                _queryFunc = queryFunc;
+                _loadedPropertyNames = loadedPropertyNames;
+            }
+
+            public override IQueryable<TEntity> Query(IQueryable<TEntity> queryable)
+                => _queryFunc(queryable);
+
+            public override IEnumerable<string> GetLoadedProperties()
+                => _loadedPropertyNames ?? base.GetLoadedProperties();
+        }
+    }
+}
---
+++ test/Ternacode.Persistence.EntityFrameworkCore.ComponentTest/Extensions/RepositoryExtensions_Test.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Ternacode.Persistence.Abstractions;
+using Ternacode.Persistence.EntityFrameworkCore.ComponentTest.Factories;
+using Ternacode.Persistence.EntityFrameworkCore.ComponentTest.Model;
+using Ternacode.Persistence.Extensions;
+
+namespace Ternacode.Persistence.EntityFrameworkCore.ComponentTest.Extensions
+{
+    public class RepositoryExtensions_Test : BaseComponentTest
+    {
+        [Test]
+        public async Task When_querying_async_with_queryFunc_Then_the_expected_entity_is_returned()
+        {
+            const string expectedName = "foo1";
+
+            var foo1 = FooFactory.Create(fooName: expectedName);
+            var foo2 = FooFactory.Create(fooName: "foo2");
+
+            var fooRepository = _serviceProvider.GetService<IRepository<Foo>>();
+            
+            await fooRepository.AddAsync(foo1);
+            await fooRepository.AddAsync(foo2);
+
+            var result = await fooRepository.QueryAsync(q => q.Where(f => f.Name == expectedName));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Count(), Is.EqualTo(1));
+                Assert.That(result.FirstOrDefault()?.Name, Is.EqualTo(expectedName));
+                Assert.That(result.FirstOrDefault()?.Bar, Is.Null);
+            });
+        }
+
+        [Test]
+        public async Task When_querying_async_with_queryFunc_and_loaded_proerties_Then_the_expected_entity_is_returned()
+        {
+            const string expectedFooName = "foo1";
+            const string expectedBarName = "bar1";
+
+            var foo1 = FooFactory.Create(fooName: expectedFooName, barName: expectedBarName);
+            var foo2 = FooFactory.Create(fooName: "foo2");
+
+            var fooRepository = _serviceProvider.GetService<IRepository<Foo>>();
+
+            await fooRepository.AddAsync(foo1);
+            await fooRepository.AddAsync(foo2);
+
+            var result = await fooRepository.QueryAsync(
+                queryFunc: q => q.Where(f => f.Name == expectedFooName),
+                loadedPropertyNames: new []{ nameof(foo1.Bar) });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Count(), Is.EqualTo(1));
+                Assert.That(result.FirstOrDefault()?.Name, Is.EqualTo(expectedFooName));
+                Assert.That(result.FirstOrDefault()?.Bar?.Name, Is.EqualTo(expectedBarName));
+            });
+        }
+    }
+}
